0ac1583159cd7ae9d687e02b95e2c27102b81a90
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-02-15 17:00:27 +0100

    add controller and model apartment

---
+++ database/seeds/ApartmentSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Apartment;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+
+class ApartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+    }
+}
