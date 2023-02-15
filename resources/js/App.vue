49ae5b5266947cb1f3bcf7fdbe138496702d4cfa
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-02-15 12:23:54 +0100

    fix vue

---
+++ resources/js/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <header>
+            <Navbar/>
+        </header>
+
+        <main>
+            <router-view></router-view>
+        </main>
+
+        <footer>
+            <Footer/>
+        </footer>
+    </div>
+</template>
+
+<script>
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+//   components: {
+//     Navbar,
+//     Footer,
+//   },
+}
+</script>
+
+<style lang="scss">
+    @import '~bootstrap/scss/bootstrap';
+
+</style>
+
--- resources/js/front.js
+++ resources/js/front.js
@@ -1,1 +1,13 @@
 require("./common");
+
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+});
