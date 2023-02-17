4fc1f13439fa37f0e784cb0477b9b761578b7cbe
nicolabzz <nbuzzigoli@gmail.com>
2023-02-17 16:04:28 +0100

    Services in Mig. done

--- database/migrations/2023_02_16_113727_create_sponsorships_table.php
+++ database/migrations/2023_02_16_113727_create_sponsorships_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateSponsorshipsTable extends Migration
 {
@@ -15,6 +16,24 @@ class CreateSponsorshipsTable extends Migration
             $table->unsignedTinyInteger('sponsor_time');
             $table->timestamps();
         });
+
+        DB::table('sponsorships')->insert([
+            [
+                'type' => 'Bronze Sponsorship',
+                'price' => 2.99,
+                'sponsor_time' => 24,
+            ],
+            [
+                'type' => 'Silver Sponsorship',
+                'price' => 5.99,
+                'sponsor_time' => 72,
+            ],
+            [
+                'type' => 'Gold Sponsorship',
+                'price' => 9.99,
+                'sponsor_time' => 144,
+            ]
+        ]);
     }
 
 
