73042e968efed9856652d13eb7cc3323913e4f41
nicolabzz <nbuzzigoli@gmail.com>
2023-02-17 10:05:26 +0100

    test problem 1

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,31 +14,31 @@ class UserSeeder extends Seeder
     public function run()
     {
         $users = [
-            [
-                'name' => 'Paperino',
-                'surname' => 'Paperino',
-                'birthdate' => '2001-01-01',
-                'email' => 'paperino@gmail.com',
-                'password' => Hash::make('paperino'),
-            ],
-            [
-                'name' => 'Pippo',
-                'surname' => 'Pippo',
-                'birthdate' => '2001-01-01',
-                'email' => 'pippo@gmail.com',
-                'password' => Hash::make('pippo'),
-            ],
-            [
-                'name' => 'Topolino',
-                'surname' => 'Topolino',
-                'birthdate' => '2001-01-01',
-                'email' => 'topolino@gmail.com',
-                'password' => Hash::make('topolino'),
-            ],
+            // [
+            //     'name' => 'Paperino',
+            //     'surname' => 'Paperino',
+            //     'birthdate' => '2001-01-01',
+            //     'email' => 'paperino@gmail.com',
+            //     'password' => Hash::make('paperino'),
+            // ],
+            // [
+            //     'name' => 'Pippo',
+            //     'surname' => 'Pippo',
+            //     'birthdate' => '2001-01-01',
+            //     'email' => 'pippo@gmail.com',
+            //     'password' => Hash::make('pippo'),
+            // ],
+            // [
+            //     'name' => 'Topolino',
+            //     'surname' => 'Topolino',
+            //     'birthdate' => '2001-01-01',
+            //     'email' => 'topolino@gmail.com',
+            //     'password' => Hash::make('topolino'),
+            // ],
         ];
 
-        foreach ($users as $user) {
-            User::create($user);
-        };
+        // foreach ($users as $user) {
+        //     User::create($user);
+        // };
     }
 }
