68db76f4458d1da3e52cca52e116e59db7cae0f1
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-02-22 15:39:06 +0100

    fix show message

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -32,25 +32,3 @@
         <a href="{{ route('admin.dashboard') }}" class="btn btn-info" data-id="{{ $message->slug}}">Back to dashboard</a>
     </div>
 @endsection
-@extends('layouts.app')
-
-@section('content')
-    @if ($message)
-        <div class="card text-bg-dark w-75 m-auto">
-            <img src="{{ $message->picture }}" class="card-img" alt="{{ $message->title }}">
-            <div class="card-img-overlay">
-                <div class="card-text">message text: {{ $message->message }}</div>
-                <div class="card-text">Date: {{ $message->date }}</div>
-            </div>
-        </div>
-    @else
-    <div>
-        <img class="d-flex m-auto" src="https://media.tenor.com/OTzJy4d4xGMAAAAC/computer-stick-man.gif" alt="gif">
-    </div>
-    @endif
-
-    <div class="controls mt-3">
-        <button class="btn btn-danger btn_delete" data-id="{{ $message->slug}}">Delete</button>
-        <a href="{{ route('admin.messages.index', ['message' => $message]) }}" class="btn btn-info" data-id="{{ $message->slug}}">Turn back</a>
-    </div>
-@endsection
