8ba7ff1ac2b13d573effb6ebcb979a43d0aa6a70
nicolabzz <nbuzzigoli@gmail.com>
2023-02-24 18:09:50 +0100

    TOMTOM homepage.blade.php (N working)  🗺️ 🔭

--- resources/views/guest/homepage.blade.php
+++ resources/views/guest/homepage.blade.php
@@ -6,17 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
     <script src="{{ asset('js/front.js') }}" defer></script>
-    {{-- style tomtom --}}
+
+    {{-- TOMTOM --}}
+    <!-- Replace version in the URL with desired library version -->
     <link
-      rel="stylesheet"
-      type="text/css"
-      href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.23.0/maps/maps.css"
+    rel="stylesheet"
+    type="text/css"
+    href="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.23.0/maps/maps.css"
     />
-    {{-- script tomtom --}}
-    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.23.0/maps/maps-web.min.js"></script>
+
+    <style>
+        #map {
+            width: 800px;
+            height: 380px;
+        }
+    </style>
+    {{-- / TOMTOM --}}
+
 </head>
+
 <body>
+
     <div id="app"></div>
+
+
+{{--
+    PARTE DI TOMTOM - GESTIONE DELLA MAPPA DEL NAVIGATORE
+--}}
+
+    <div id="map" class="map"></div>
+
+
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.23.0/maps/maps-web.min.js"></script>
+
+    <script type="text/javascript">
+
+        let center = [9.135752487471335, 45.46166580610122] //TODO: mettere latitude, longitude come variabili
+
+        const map = tt.map({
+            key: "gMfd6J6PIRqQQaAmyKd2WQbENA4FkXwr",
+            container: "map",
+            center: center,
+            zoom: 17,
+        })
+        map.on('load', () => {
+                    var marker = new tt.Marker().setLngLat(center).addTo(map)
+                })
+
+    </script>
+
+{{--
+    / PARTE DI TOMTOM - GESTIONE DELLA MAPPA DEL NAVIGATORE
+--}}
 </body>
 </html>
-
