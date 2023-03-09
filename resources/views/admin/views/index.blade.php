1afd9dbd7d4e26582ff57b2dfc0204609bf6b4b7
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-03-09 10:56:46 +0100

    init statistics on views

--- resources/views/admin/views/index.blade.php
+++ resources/views/admin/views/index.blade.php
@@ -8,41 +8,71 @@
             <a href="{{ route('admin.dashboard') }}" class="btn btn-info">Back to dashboard</a>
         </div>
     @else
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Apartment ID</th>
-                <th scope="col">User</th>
-                <th scope="col">Address</th>
-                <th scope="col">City</th>
-                <th scope="col">Views</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($apartments as $apartment)
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <th scope="row">{{ $apartment->title }}</th>
-                    <td>{{ $apartment->user->name }}</td>
-                    <td>{{ $apartment->address }}</td>
-                    <td>{{ $apartment->city }}</td>
-                    <td>
-                        <ul>
-                            @foreach ($apartment->views as $view)
-                                <li>IP address: {{ $view->IP }}</li>
-                            @endforeach
-                        </ul>
-                    </td>
+                    <th scope="col">Apartment ID</th>
+                    <th scope="col">User</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">City</th>
+                    <th scope="col">Views</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($apartments as $apartment)
+                    <tr>
+                        <th scope="row">{{ $apartment->title }}</th>
+                        <td>{{ $apartment->user->name }}</td>
+                        <td>{{ $apartment->address }}</td>
+                        <td>{{ $apartment->city }}</td>
+                        <td>
+                            <ul>
+                                @foreach ($apartment->views as $view)
+                                    <li>IP address: {{ $view->IP }}</li>
+                                @endforeach
+                            </ul>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endif
     @if ($views->isEmpty())
         <p></p>
     @else
-    {{ $views->links() }}
+        {{ $views->links() }}
+        <div>
+            <canvas id="myChart"></canvas>
+        </div>
         <div>
             <a href="{{ route('admin.dashboard') }}" class="btn btn-info" data-id="{{ $apartment->slug}}">Back to dashboard</a>
         </div>
     @endif
+
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+        const ctx = document.getElementById('myChart');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+            },
+            options: {
+            scales: {
+                y: {
+                beginAtZero: true
+                }
+            }
+            }
+        });
+    </script>
+
 @endsection
